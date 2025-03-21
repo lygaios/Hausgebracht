04251f32ca360dab3875a6b2a21b005b90f14e9c
Joshua Brunke <seafortghost@gmail.com>
2025-03-21 11:03:00 +0100

    Added files

---
+++ scripts/db.js
@@ -0,0 +1,29 @@
+let dishes = [
+    {
+      "name": "Zwiebelringe",
+      "price": 8.99
+    },
+    {
+        "name": "Chicken Wings",
+        "price": 12.99
+      },
+      {
+        "name": "Club Sandwich",
+        "price": 10.99
+      },
+      {
+        "name": "Hamburger mit Pommes Frites",
+        "price": 14.99
+      },
+      {
+        "name": "Pizza Mexicana",
+        "price": 12.99
+      },   
+      {
+        "name": "Obstsalat California",
+        "price": 9.99
+      }        
+    ]
+
+
+let cart = []
