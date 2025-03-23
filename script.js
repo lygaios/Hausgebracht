49c483e6d43392bd8c7df6ca6f8a925052ceff88
Joshua Brunke <seafortghost@gmail.com>
2025-03-23 16:15:22 +0100

    Added menu display structure

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function init() {
 function showCartOverlay() {
     document.getElementById("overlay").classList.remove("dnone");
 }
+
+function renderDishes() {
+    let dishesRef = document.getElementById("menu-contents");
+    dishesRef.innerHTML = "";
+    for (let i = 0; i < dishes.length; i++) {
+        dishesRef.innerHTML += getDishesTemplate(i);
+    }
+}
+
+function getDishesTemplate(i) {
+    return ` 
+    <div class="dishes-box">
+        <div class="data-block">
+            <h2>${dishes[i].name}</h2>
+            <p>${dishes[i].price.toFixed(2)}</p>
+        </div>
+        <div class="interact-row"></div>
+    </div>`
+}
--- scripts/db.js
+++ scripts/db.js
@@ -1,27 +1,33 @@
 let dishes = [
     {
       "name": "Zwiebelringe",
-      "price": 8.99
+      "price": 8.99,
+      "amount": 0
     },
     {
         "name": "Chicken Wings",
-        "price": 12.99
+        "price": 12.99,
+        "amount": 0
       },
       {
         "name": "Club Sandwich",
-        "price": 10.99
+        "price": 10.99,
+        "amount": 0
       },
       {
         "name": "Hamburger mit Pommes Frites",
-        "price": 14.99
+        "price": 14.99,
+        "amount": 0
       },
       {
         "name": "Pizza Mexicana",
-        "price": 12.99
+        "price": 12.99,
+        "amount": 0
       },   
       {
         "name": "Obstsalat California",
-        "price": 9.99
+        "price": 9.99,
+        "amount": 0
       }        
     ]
 
