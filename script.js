de7c5d32c5340057733e04cc9359a193f58cd44c
Joshua Brunke <seafortghost@gmail.com>
2025-03-24 09:19:14 +0100

    Started cart basics, not functional yet

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ function renderDishes() {
     dishesRef.innerHTML = "";
     for (let i = 0; i < dishes.length; i++) {
         dishesRef.innerHTML += getDishesTemplate(i);
+        renderCart(i, j);
+    }
+}
+
+function renderCart(i, j) {
+    if (document.getElementById("overlay").classList.contains("dnone") == false) {
+        let cartRef = document.getElementById("overlay-cart");
+        for (let j = 0; j < cart.length; j++) {
+            cartRef.innerHTML += getCartTemplate(i, j);
+        }
+    } else {let cartRef = document.getElementById("sidebar-cart");
+        for (let j = 0; j < cart.length; j++) {
+            cartRef.innerHTML += getCartTemplate(i, j);
+    }
     }
 }
 
@@ -22,17 +36,52 @@ function getDishesTemplate(i) {
             <p>${dishes[i].price.toFixed(2)} €</p>
         </div>
         <div class="interact-row">
-            <button id="less" class="button" onclick "removeDish()">-</button>
-            <button id="more" class="button" onclick "addDish()">+</button>
+            <button class="button" onclick "removeDish(i, j)">-</button>
+            <p>${dishes[j].amount}</p> 
+            <button class="button" onclick "addDish(i, j)">+</button>
         </div>
     </div>
     `;
 }
 
-function removeDish() {
+function addDish(i, j) {
+    if (dishes[j].amount = 0) {
+
+        let dishesNumber = dishes[j].amount + 1;
+        dishes[j].amount = dishesNumber;
+        renderDishes(i)
+    }
+}
+
+function removeDish(i, j) {
+    if (dishes[j].amount > 0) {
+        let dishesNumber = dishes[j].amount - 1;
+        dishes[j].amount = dishesNumber;
+        renderDishes(i, j)
+    }
+}
+
+
+
+function getCartTemplate(i, j){
+    if (cart.amount > 0) {
+        return ` 
+        <div class="cart-box">
+            <div class="data-block">
+             <h3>${cart[j].name}</h2>
+                <p>${cart[j].price.toFixed(2)} €</p>
+            </div>
+            <div class="interact-row">
+                <button class="button" onclick "removeDish(i, j)">-</button>
+                <p>${cart[j].amount}</p> 
+                <button class="button" onclick "addDish(i, j)">+</button>
+            </div>
+    </div>
+    `;
+    }
+}
+
+function renderTotalAmount(i, j) {
 
 }
 
-function addDish() {
-    
-}
