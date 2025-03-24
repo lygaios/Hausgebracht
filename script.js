1a499baf16a196ab72c7cc45801e0f40c6f75a05
Joshua Brunke <seafortghost@gmail.com>
2025-03-24 17:21:09 +0100

    Moved templates to templates file, added experimental cart rendering code (not functional yet)

--- script.js
+++ script.js
@@ -10,76 +10,71 @@ function closeOverlay() {
     document.getElementById("overlay").classList.add("dnone"); 
 }
 
+function getValueFromInput(id) {
+    let value = document.getElementById(id).value;
+    return value;
+}
+
 function renderDishes() {
-  let dishesRef = document.getElementById("menu-contents");
-  dishesRef.innerHTML = "";
-  for (let i = 0; i < dishes.length; i++) {
-    dishesRef.innerHTML += getDishesTemplate(i);
-    renderCart(i)
-  }
-}
-
-function renderCart(i) {
-if (document.getElementById("overlay").classList.contains("dnone") == false) {
-    let cartRef = document.getElementById("overlay-cart");
-    for (let j = 0; j < cart.length; j++) {
-      cartRef.innerHTML += getCartTemplate(i, j);
-    }
-  } else {
-    let cartRef = document.getElementById("sidebar-cart");
-    for (let j = 0; j < cart.length; j++) {
-      cartRef.innerHTML += getCartTemplate(i, j);
+    let dishesRef = document.getElementById("menu-contents");
+    dishesRef.innerHTML = "";
+    for (let i = 0; i < dishes.length; i++) {
+      dishesRef.innerHTML += getDishesTemplate(i);
+      renderCart(i);
     }
   }
+  
+function renderSidebarCart(i) {
+    if (document.getElementById("overlay").classList.contains("dnone")) {
+        let cartRef = document.getElementById("sidebar-cart");}
+    }
+
+
+function renderOverlayCart(i) {
+    if (document.getElementById("overlay").classList.contains("dnone") == false) {
+        let cartRef = document.getElementById("overlay-cart");}
 }
 
-function getDishesTemplate(i) {
-  return ` 
-    <div class="dishes-box">
-        <div class="data-block">
-            <h3>${dishes[i].name}</h2>
-            <p>${dishes[i].price.toFixed(2)} €</p>
-        </div>
-        <div class="interact-row">
-            <button class="button" onclick "removeDish(i, j)">-</button>
-   
-            <button class="button" onclick "addDish(i, j)">+</button>
-        </div>
-    </div>
-    `;
-}
-
-function addDish(i, j) {
-  if (cart[j].amount = 0) {
-    let cartNumber = cart[j].amount + 1;
-    cart[j].amount = cartNumber;
-
-    renderDishes(i, j);
+/*
+  function renderCart(i, j) {
+  if (document.getElementById("overlay").classList.contains("dnone") == false) {
+      let cartRef = document.getElementById("overlay-cart");
+      for (let j = 0; j < cart.length; j++) {
+        cartRef.innerHTML += getCartTemplate(i, j);
+        renderDishes(i, j)
+      }
+    } else {
+      let cartRef = document.getElementById("sidebar-cart");
+      for (let j = 0; j < cart.length; j++) {
+        cartRef.innerHTML += getCartTemplate(i, j);
+        renderDishes(i, j)
+      }
+    }
   }
-}
-
-function removeDish(i, j) {
-  if (cart[j].amount > 0) {
-    let cartNumber = cart[j].amount - 1;
-    cart[j].amount = cartNumber;
-    renderDishes(i, j);
+    */
+  
+  
+  function addDish(i, j) {
+    if (cart[j].amount = 0) {
+  let newCartDish = dishes[i];
+  cart.push(newCartDish.innerHTML);
+  let cartNumber = cart[j].amount + 1;
+  cart[j].amount = cartNumber;
+      renderDishes(i, j);
+      renderCart(i, j)
+    }
   }
-}
-
-function getCartTemplate(i, j) {
-  if (cart.amount > 0) {
-    return ` 
-        <div class="cart-box">
-            <div class="data-block">
-             <h3>${cart[j].name}</h2>
-                <p>${cart[j].price.toFixed(2)} €</p>
-            </div>
-            <div class="interact-row">
-                <button class="button" onclick "removeDish(i, j)">-</button>
-                <p>${cart[j].amount}</p> 
-                <button class="button" onclick "addDish(i, j)">+</button>
-            </div>
-    </div>
-    `;
+  
+  function removeDish(i, j) {
+    if (cart[j].amount > 1) {
+      let cartNumber = cart[j].amount - 1;
+      cart[j].amount = cartNumber;
+      renderDishes(i, j);
+    } else if (cart[j].amount == 1) {
+        cart.splice(j, 1);
+        renderDishes(i, j);
+        renderCart(i, j)
+    }
   }
-}
+  
+  
