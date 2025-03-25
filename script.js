ffd46ece14067a8ac9be2ec37478240f5bf27511
Joshua Brunke <seafortghost@gmail.com>
2025-03-25 10:59:38 +0100

    Reworked code, not functional yet

--- script.js
+++ script.js
@@ -10,10 +10,7 @@ function closeOverlay() {
     document.getElementById("overlay").classList.add("dnone"); 
 }
 
-function getValueFromInput(id) {
-    let value = document.getElementById(id).value;
-    return value;
-}
+
 
 function renderDishes() {
     let dishesRef = document.getElementById("menu-contents");
@@ -21,61 +18,45 @@ function renderDishes() {
     for (let i = 0; i < dishes.length; i++) {
       dishesRef.innerHTML += getDishesTemplate(i);
 
-      //renderCart(i);
+      renderSidebarCart(i);
+      renderOverlayCart(i);
     }
   }
   
 function renderSidebarCart(i) {
-    if (document.getElementById("overlay").classList.contains("dnone")) {
-        let cartRef = document.getElementById("sidebar-cart");}
-    }
-
-
-function renderOverlayCart(i) {
-    if (document.getElementById("overlay").classList.contains("dnone") == false) {
-        let cartRef = document.getElementById("overlay-cart");}
+  let sidebarCartRef = document.getElementById("sidebar-cart");
+  sidebarCartRef.innerHTML = "";
+  for (let i = 0; i < cart.length; i++) {
+    sidebarCartRef.innerHTML += getCartTemplate(i);
+  }
 }
 
-/*
-  function renderCart(i, j) {
-  if (document.getElementById("overlay").classList.contains("dnone") == false) {
-      let cartRef = document.getElementById("overlay-cart");
-      for (let j = 0; j < cart.length; j++) {
-        cartRef.innerHTML += getCartTemplate(i, j);
-        renderDishes(i, j)
-      }
-    } else {
-      let cartRef = document.getElementById("sidebar-cart");
-      for (let j = 0; j < cart.length; j++) {
-        cartRef.innerHTML += getCartTemplate(i, j);
-        renderDishes(i, j)
-      }
-    }
+function renderOverlayCart(i) {
+  let overlayCartRef = document.getElementById("overlay-cart");
+  overlayCartRef.innerHTML = "";
+  for (let i = 0; i < cart.length; i++) {
+    overlayCartRef.innerHTML += getCartTemplate(i);
   }
-    */
+}
+
   
-  
-  function addDish(i, j) {
-    if (cart[j].amount = 0) {
-  let newCartDish = dishes[i];
-  cart.push(newCartDish.innerHTML);
-  let cartNumber = cart[j].amount + 1;
-  cart[j].amount = cartNumber;
-      renderDishes(i, j);
-      renderCart(i, j)
-    }
+  function addDish(i) {
+  let newCartDishName = dishes[i].name;
+  let newCartDishPrice = dishes[i].price;
+  let newCartDishAmount = dishes[i].amount + 1;
+
+let newCartDish = {name : newCartDishName, price : newCartDishPrice, amount : newCartDishAmount};
+
+if (cart[i].newCartDishAmount == 1) {
+  cart.push(newCartDish);
+} else {
+  cart[i].amount++;
+}
+      renderDishes(i);
+      renderSidebarCart(i);
+      renderOverlayCart(i);
   }
   
-  function removeDish(i, j) {
-    if (cart[j].amount > 1) {
-      let cartNumber = cart[j].amount - 1;
-      cart[j].amount = cartNumber;
-      renderDishes(i, j);
-    } else if (cart[j].amount == 1) {
-        cart.splice(j, 1);
-        renderDishes(i, j);
-        renderCart(i, j)
-    }
-  }
+
   
   
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,4 +1,4 @@
-function getDishesTemplate(i, j) {
+function getDishesTemplate(i) {
     return ` 
       <div class="dishes-box">
           <div class="data-block">
@@ -6,30 +6,28 @@ function getDishesTemplate(i, j) {
               <p>${dishes[i].price.toFixed(2)} €</p>
           </div>
           <div class="interact-row">
-              <button class="button" onclick "removeDish(i, j)">-</button> 
-              <button class="button" onclick "addDish(i, j)">+</button>
+              <button class="button" onclick "removeDish(i)">-</button> 
+              <button class="button" onclick "addDish(i)">+</button>
           </div>
       </div>
       `;
   }
   
-  function getCartTemplate(i, j) {
-      if (cart.amount > 0) {
-        return ` 
-            <div class="cart-box">
-             <h2>Warenkorb</h2>
-                <div class="data-block">
-                 <h3>${cart[j].name}</h2>
-                    <p>${cart[j].price.toFixed(2)} €</p>
-                </div>
-                <div class="interact-row">
-                    <button class="button" onclick "removeDish(i, j)">-</button>
-                    <p>${cart[j].amount}</p> 
-                    <button class="button" onclick "addDish(i, j)">+</button>
-                </div>
-        </div>
-        `;
-      }
-    }
+  function getCartTemplate(i) {
+      return ` 
+          <div class="cart-box">
+              <div class="data-block">
+               <h3>${cart[i].name}</h2>
+                  <p>${cart[i].price.toFixed(2)} €</p>
+              </div>
+              <div class="interact-row">
+                  <button class="button" onclick "removeDish(i)">-</button>
+                  <p>${cart[i].amount}</p> 
+                  <button class="button" onclick "addDish(i)">+</button>
+              </div>
+      </div>
+      `;
+  }
+
   
   
