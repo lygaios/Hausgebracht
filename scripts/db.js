4a5760b9cf8e5b2bd56221ba125bee97bd8edacb
Joshua Brunke <seafortghost@gmail.com>
2025-03-26 19:31:12 +0100

    Added ids to database items

--- scripts/db.js
+++ scripts/db.js
@@ -1,30 +1,36 @@
 let dishes = [
   {
+    id: 1,
     name: "Zwiebelringe",
     price: 8.99,
     amount: 0,
   },
   {
+    id: 2,
     name: "Chicken Wings",
     price: 12.99,
     amount: 0,
   },
   {
+    id: 3,
     name: "Club Sandwich",
     price: 10.99,
     amount: 0,
   },
   {
+    id: 4,
     name: "Hamburger Classic",
     price: 14.99,
     amount: 0,
   },
   {
+    id: 5,
     name: "Pizza Mexicana",
     price: 12.99,
     amount: 0,
   },
   {
+    id: 6,
     name: "Obstsalat California",
     price: 9.99,
     amount: 0,
